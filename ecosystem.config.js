398c2d77333655e6028022ee514c366acce9874e
garzj <johannes@garz.dev>
2024-04-23 12:59:32 +0200

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'wa-announcements-bot',
+      script: 'build/index.js',
+      merge_logs: true,
+      max_restarts: 5,
+      instances: 1,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
